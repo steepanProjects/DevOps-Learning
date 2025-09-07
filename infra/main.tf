c38e71706ed486b88ed73bbbaaca89ad2a73f1d9
steepanProjects <steepan430@gmail.com>
2025-09-07 14:34:43 +0530

    test 12

--- infra/main.tf
+++ infra/main.tf
@@ -12,25 +12,115 @@ provider "aws" {
   region = var.aws_region
 }
 
-
-
+##########################
 # ECS Cluster
+##########################
 resource "aws_ecs_cluster" "this" {
   name = "flask-cluster"
 }
 
-# ECS Task Execution IAM Role (existing)
+##########################
+# ECS Task Execution Role
+##########################
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
 
-# Attach policy (optional)
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = data.aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+##########################
+# Security Group (ALB)
+##########################
+data "aws_security_group" "existing_alb_sg" {
+  count  = length(try([for sg in aws_security_groups : sg if sg.name == "alb-sg"], []))
+  name   = "alb-sg"
+  vpc_id = "vpc-0520ee24692866295"
+}
+
+resource "aws_security_group" "alb_sg" {
+  count       = length(data.aws_security_group.existing_alb_sg) == 0 ? 1 : 0
+  name        = "alb-sg"
+  description = "Allow HTTP"
+  vpc_id      = "vpc-0520ee24692866295"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+locals {
+  alb_sg_id = length(data.aws_security_group.existing_alb_sg) > 0 ? data.aws_security_group.existing_alb_sg[0].id : aws_security_group.alb_sg[0].id
+}
+
+##########################
+# Target Group
+##########################
+data "aws_lb_target_group" "existing_flask_tg" {
+  count = length(try([for tg in aws_lb_target_groups : tg if tg.name == "flask-tg"], []))
+  name  = "flask-tg"
+}
+
+resource "aws_lb_target_group" "flask_tg" {
+  count       = length(data.aws_lb_target_group.existing_flask_tg) == 0 ? 1 : 0
+  name        = "flask-tg"
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = "vpc-0520ee24692866295"
+  target_type = "ip"
+}
+
+locals {
+  flask_tg_arn = length(data.aws_lb_target_group.existing_flask_tg) > 0 ? data.aws_lb_target_group.existing_flask_tg[0].arn : aws_lb_target_group.flask_tg[0].arn
+}
+
+##########################
+# ALB
+##########################
+resource "aws_lb" "flask_alb" {
+  name               = "flask-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [local.alb_sg_id]
+  subnets            = ["subnet-0e5c0731ddedc24ae","subnet-0002fdfcb821db040"]
+}
+
+##########################
+# Listener
+##########################
+data "aws_lb_listener" "existing_listener" {
+  count = length(try([for l in aws_lb_listeners : l if l.load_balancer_arn == aws_lb.flask_alb.arn], []))
+  load_balancer_arn = aws_lb.flask_alb.arn
+  port              = 80
+}
+
+resource "aws_lb_listener" "flask_listener" {
+  count             = length(data.aws_lb_listener.existing_listener) == 0 ? 1 : 0
+  load_balancer_arn = aws_lb.flask_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = local.flask_tg_arn
+  }
+}
+
+##########################
 # ECS Task Definition
+##########################
 resource "aws_ecs_task_definition" "flask_app" {
   family                   = "flask-task"
   requires_compatibilities = ["FARGATE"]
@@ -50,58 +140,9 @@ resource "aws_ecs_task_definition" "flask_app" {
   }])
 }
 
-# Security group for ALB
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP"
-  vpc_id      = "vpc-0520ee24692866295" 
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# ALB
-resource "aws_lb" "flask_alb" {
-  name               = "flask-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = ["subnet-0e5c0731ddedc24ae","subnet-0002fdfcb821db040"]
-}
-
-# Target Group
-resource "aws_lb_target_group" "flask_tg" {
-  name        = "flask-tg"
-  port        = 5000
-  protocol    = "HTTP"
-  vpc_id      = "vpc-0520ee24692866295"
-  target_type = "ip"
-}
-
-# Listener
-resource "aws_lb_listener" "flask_listener" {
-  load_balancer_arn = aws_lb.flask_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.flask_tg.arn
-  }
-}
-
-# ECS Service with ALB integration
+##########################
+# ECS Service
+##########################
 resource "aws_ecs_service" "flask_service" {
   name            = "flask-service"
   cluster         = aws_ecs_cluster.this.id
@@ -111,12 +152,12 @@ resource "aws_ecs_service" "flask_service" {
 
   network_configuration {
     subnets         = ["subnet-0e5c0731ddedc24ae","subnet-0002fdfcb821db040"]
-    security_groups = ["sg-07c108c9c401c1db5"]
+    security_groups = [local.alb_sg_id]
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.flask_tg.arn
+    target_group_arn = local.flask_tg_arn
     container_name   = "flask-container"
     container_port   = 5000
   }
