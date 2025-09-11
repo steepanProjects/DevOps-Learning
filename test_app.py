533b4dac1dd232260ad0e80c5277fe2dca92efcf
steepanProjects <steepan430@gmail.com>
2025-09-11 09:52:48 +0530

    test 15

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello from Steepan's DevOps Project.!" in response.data
+    assert b"Hello from Steepan's DevOps Project..!" in response.data
